535e32ac648612dde4f154956a2bc8372a91d0d4
Dong Zeng <zeng-dong@hotmail.com>
2022-11-20 10:03:18 -0600

    use state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import './App.css';
 import EnrolmentForm from './EnrolmentForm';
 
 function App() {
-    const handleClick = () => {
-        alert('Start Reacting');
-    };
     return (
         <div className="App">
-            <button onClick={handleClick}>Just React</button>
-            <hr />
             <EnrolmentForm />
         </div>
     );
--- src/EnrolmentForm.js
+++ src/EnrolmentForm.js
@@ -1,18 +1,41 @@
+import { useState } from 'react';
 import './App.css';
+
 function EnrolmentForm() {
+    const [firstName, setFirstName] = useState('m');
+    const [lastName, setLastName] = useState('z');
+    const [welcomeMessage, setWelcomeMessage] = useState('');
+
+    const handleSubmit = (event) => {
+        setWelcomeMessage(`Welcome ${firstName} ${lastName}`);
+        event.preventDefault();
+    };
+
     return (
         <div>
-            <form className="enrolForm">
+            <form className="enrolForm" onSubmit={handleSubmit}>
                 <h1>Student Details</h1>
                 <label>First name:</label>
-                <input type="text" name="fname" />
+                <input
+                    type="text"
+                    name="fname"
+                    onBlur={(event) => setFirstName(event.target.value)}
+                />
                 <br />
                 <label>Last name:</label>
-                <input type="text" name="lname" />
+                <input
+                    type="text"
+                    name="lname"
+                    onBlur={(event) => setLastName(event.target.value)}
+                />
                 <br />
                 <br />
                 <input type="submit" value="Submit" />
             </form>
+
+            <label id="studentMsg" className="message">
+                {welcomeMessage}
+            </label>
         </div>
     );
 }
