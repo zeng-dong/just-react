db4d5c3eb653552572cda2cd16f2335b4367abf1
Dong Zeng <zeng-dong@hotmail.com>
2022-11-21 20:08:43 -0600

    parent now have state and prepares func to pass down

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import EnrolmentForm from './EnrolmentForm';
 
 function App() {
     const [program, setProgram] = useState('UG');
+    const [seats, setSeats] = useState(100);
     const handleChange = (event) => {
         setProgram(event.target.value);
+    };
+    const setUpdatedSeats = (updatedSeats) => {
+        setSeats(updatedSeats);
     };
 
     return (
         <div className="App">
             <div className="programs">
+                <label>Remaining Seats - {seats}</label>
+                <br />
+                <br />
                 <label>Choose Program:</label>
                 <select
                     className="appDropDowns"
@@ -21,7 +28,11 @@ function App() {
                     <option value="PG">Postgraduate</option>
                 </select>
             </div>
-            <EnrolmentForm chosenProgram={program} />
+            <EnrolmentForm
+                chosenProgram={program}
+                setUpdatedSeats={setUpdatedSeats}
+                currentSeats={seats}
+            />
         </div>
     );
 }
