d326dad3f46d7b76549487c797710f24feaaa91e
Dong Zeng <zeng-dong@hotmail.com>
2022-11-22 21:37:11 -0600

    styling and refactor

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { useState } from 'react';
 import './App.css';
 import EnrolmentForm from './EnrolmentForm';
 
-function App() {
+const App = () => {
     const [program, setProgram] = useState('UG');
     const [ugSeats, setUgSeats] = useState(60);
     const [pgSeats, setPgSeats] = useState(40);
 
     const handleChange = (event) => {
         setProgram(event.target.value);
+        setPgSeats(pgSeats);
+        setUgSeats(ugSeats);
     };
 
     const setUpdatedSeats = (updatedSeats) => {
@@ -22,21 +24,31 @@ function App() {
     return (
         <div className="App">
             <div className="programs">
-                <label>Remaining UG Seats - {ugSeats}</label>
-                <br />
-                <br />
-                <label>Remaining PG Seats - {pgSeats}</label>
-                <br />
-                <br />
-                <label>Choose Program:</label>
-                <select
-                    className="appDropDowns"
-                    onChange={handleChange}
-                    value={program}
-                >
-                    <option value="UG">Undergraduate</option>
-                    <option value="PG">Postgraduate</option>
-                </select>
+                <h3 className="title">Student Enrolment Form</h3>
+                <ul className="ulEnrol">
+                    <li className="parentLabels" onChange={handleChange}>
+                        <input
+                            type="radio"
+                            value="UG"
+                            name="programGroup"
+                            defaultChecked
+                        />
+                        Undergraduate
+                        <input
+                            type="radio"
+                            value="PG"
+                            name="programGroup"
+                            className="radioSel"
+                        />
+                        Postgraduate
+                    </li>
+                    <li>
+                        <label className="parentLabels">
+                            Remaining {program} Seats -{' '}
+                            {program === 'UG' ? ugSeats : pgSeats}
+                        </label>
+                    </li>
+                </ul>
             </div>
             <EnrolmentForm
                 chosenProgram={program}
@@ -45,5 +57,5 @@ function App() {
             />
         </div>
     );
-}
+};
 export default App;
--- src/EnrolmentForm.js
+++ src/EnrolmentForm.js
@@ -1,43 +1,96 @@
 import { useState } from 'react';
 import './App.css';
 
-function EnrolmentForm(props) {
+const EnrolmentForm = (props) => {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
     const [welcomeMessage, setWelcomeMessage] = useState('');
-
-    const handleSubmit = (event) => {
-        setWelcomeMessage(`Welcome ${firstName} ${lastName}`);
+    const handleClick = (event) => {
+        handleInputReset('', '', '');
+        setWelcomeMessage(
+            `${firstName} ${lastName} enrolled. Email sent to - ${email}`
+        );
         props.setUpdatedSeats(props.currentSeats - 1);
         event.preventDefault();
     };
-
+    //change of input value set method
+    const handleInputChange = (setInput, event) => {
+        setInput(event.target.value);
+    };
+    //set input fields
+    const handleInputReset = (fname, lname, email) => {
+        setFirstName(fname);
+        setLastName(lname);
+        setEmail(email);
+    };
     return (
         <div>
-            <form className="enrolForm" onSubmit={handleSubmit}>
-                <h1>{props.chosenProgram} Student Details</h1>
-                <label>First name:</label>
-                <input
-                    type="text"
-                    name="fname"
-                    onBlur={(event) => setFirstName(event.target.value)}
-                />
-                <br />
-                <label>Last name:</label>
-                <input
-                    type="text"
-                    name="lname"
-                    onBlur={(event) => setLastName(event.target.value)}
-                />
-                <br />
-                <br />
-                <input type="submit" value="Submit" />
-            </form>
-
-            <label id="studentMsg" className="message">
-                {welcomeMessage}
-            </label>
+            <div className="enrolContainer">
+                <form className="enrolForm" name="enrolForm">
+                    <ul className="ulEnrol">
+                        <li>
+                            <label for="firstname"></label>
+                            <input
+                                type="text"
+                                className="inputFields"
+                                id="firstname"
+                                name="firstname"
+                                placeholder="First Name"
+                                value={firstName}
+                                onChange={(event) =>
+                                    handleInputChange(setFirstName, event)
+                                }
+                            />
+                        </li>
+                        <li>
+                            <label for="lastname"></label>
+                            <input
+                                type="test"
+                                className="inputFields"
+                                id="lastname"
+                                name="lastname"
+                                placeholder="Last Name"
+                                value={lastName}
+                                onChange={(event) =>
+                                    handleInputChange(setLastName, event)
+                                }
+                            />
+                        </li>
+                        <li>
+                            <label for="email"></label>
+                            <input
+                                type="email"
+                                className="inputFields"
+                                id="email"
+                                name="email"
+                                placeholder="Email"
+                                value={email}
+                                onChange={(event) =>
+                                    handleInputChange(setEmail, event)
+                                }
+                            />
+                        </li>
+                        <li id="center-btn">
+                            <input
+                                type="submit"
+                                id="btnEnrol"
+                                name="Enrol"
+                                alt="Enrol"
+                                value="Enrol"
+                                onClick={handleClick}
+                            />
+                        </li>
+                        <li>
+                            {' '}
+                            <label id="studentMsg" className="message">
+                                {welcomeMessage}
+                            </label>
+                        </li>
+                    </ul>
+                </form>
+            </div>
         </div>
     );
-}
+};
 export default EnrolmentForm;
